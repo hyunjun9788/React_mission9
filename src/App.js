914e673de4a6d61b81d69483a4a1d73961e31bef
hyunjun9788 <namhyunjun9788@gmail.com>
2023-06-14 17:31:10 +0900

    feat: Header 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <form>
+        <div>
+          <label>Email:</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password"/>
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/layout/Header.js
@@ -0,0 +1,7 @@
+function Header(){
+    return(
+        <h1>로그인 페이지</h1>
+    )
+}
+
+export default Header
